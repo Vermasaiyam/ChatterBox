2f2d3c87538eeea9dc14a6d1f6bfd8b9fb688505
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-03 01:04:19 +0530

    populated posts on user login

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -63,15 +63,16 @@ export const login = async (req, res) => {
         const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1d' });
 
         // populate each post if in the posts array
-        // const populatedPosts = await Promise.all(
-        //     user.posts.map(async (postId) => {
-        //         const post = await Post.findById(postId);
-        //         if (post.author.equals(user._id)) {
-        //             return post;
-        //         }
-        //         return null;
-        //     })
-        // )
+        const populatedPosts = await Promise.all(
+            user.posts.map(async (postId) => {
+                const post = await Post.findById(postId);
+                if (post.author.equals(user._id)) {
+                    return post;
+                }
+                return null;
+            })
+        )
+
         user = {
             _id: user._id,
             username: user.username,
@@ -80,8 +81,7 @@ export const login = async (req, res) => {
             bio: user.bio,
             followers: user.followers,
             following: user.following,
-            // posts: populatedPosts,
-            posts: user.posts,
+            posts: populatedPosts,
         }
 
         return res.cookie('token', token, { httpOnly: true, sameSite: 'strict', maxAge: 1 * 24 * 60 * 60 * 1000 }).json({
