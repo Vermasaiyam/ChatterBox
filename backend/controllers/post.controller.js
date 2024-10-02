e2809674967c96aaff0b98980564ed711008d197
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-02 22:56:24 +0530

    post controller

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,133 @@
+import sharp from "sharp";
+import { Post } from "../models/post.model.js";
+import { User } from "../models/user.model.js";
+import cloudinary from "../utils/cloudinary.js";
+
+
+export const addNewPost = async (req, res) => {
+    try {
+        const { caption } = req.body;
+        const image = req.file;
+        const authorId = req.id;
+
+        if (!image) return res.status(400).json({ message: 'Image required' });
+
+        // image upload 
+        const optimizedImageBuffer = await sharp(image.buffer)
+            .resize({ width: 800, height: 800, fit: 'inside' })
+            .toFormat('jpeg', { quality: 80 })
+            .toBuffer();
+
+        // buffer to data uri
+        const fileUri = `data:image/jpeg;base64,${optimizedImageBuffer.toString('base64')}`;
+        const cloudResponse = await cloudinary.uploader.upload(fileUri);
+        const post = await Post.create({
+            caption,
+            image: cloudResponse.secure_url,
+            author: authorId
+        });
+        const user = await User.findById(authorId);
+        if (user) {
+            user.posts.push(post._id);
+            await user.save();
+        }
+
+        await post.populate({ path: 'author', select: '-password' });
+
+        return res.status(201).json({
+            message: 'New post created',
+            post,
+            success: true,
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getAllPost = async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ createdAt: -1 })
+            .populate({ path: 'author', select: 'username profilePicture' })
+            .populate({
+                path: 'comments',
+                sort: { createdAt: -1 },
+                populate: {
+                    path: 'author',
+                    select: 'username profilePicture'
+                }
+            });
+        return res.status(200).json({
+            posts,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getUserPost = async (req, res) => {
+    try {
+        const authorId = req.id;
+        const posts = await Post.find({ author: authorId }).sort({ createdAt: -1 })
+            .populate({
+                path: 'author',
+                select: 'username, profilePicture'
+            })
+            .populate({
+                path: 'comments',
+                sort: { createdAt: -1 },
+                populate: {
+                    path: 'author',
+                    select: 'username, profilePicture'
+                }
+            });
+        return res.status(200).json({
+            posts,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const likePost = async (req, res) => {
+    try {
+        const userId = req.id;
+        const postId = req.params.id;
+        const post = await Post.findById(postId);
+        if (!post) return res.status(404).json({ message: 'Post not found', success: false });
+
+        // like logic started
+        // $addToSet isliye use kia hai taki ek banda kitni hi brr like kre pr count 1 hi brr ho
+        await post.updateOne({ $addToSet: { likes: userId } });
+        await post.save();
+
+        // implement socket io for real time notification
+
+
+        return res.status(200).json({ message: 'Post Liked.', success: true });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const dislikePost = async (req, res) => {
+    try {
+        const userId = req.id;
+        const postId = req.params.id;
+        const post = await Post.findById(postId);
+        if (!post) return res.status(404).json({ message: 'Post not found', success: false });
+
+        // like logic started
+        await post.updateOne({ $pull: { likes: userId } });
+        await post.save();
+
+        // implement socket io for real time notification
+        
+
+        return res.status(200).json({ message: 'Post Disliked.', success: true });
+    } catch (error) {
+        console.log(error);
+    }
+}
