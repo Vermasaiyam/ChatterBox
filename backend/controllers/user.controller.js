a86b6e66fa254c9b42ce38a12caea0c134e88a2e
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-02 21:07:27 +0530

    user controller edit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -109,8 +109,7 @@ export const logout = async (req, res) => {
 export const getProfile = async (req, res) => {
     try {
         const userId = req.params.id;
-        // let user = await User.findById(userId).populate({path:'posts', createdAt:-1}).populate('bookmarks');
-        let user = await User.findById(userId);
+        let user = await User.findById(userId).populate({path:'posts', createdAt:-1}).populate('bookmarks');
         return res.status(200).json({
             user,
             success: true
