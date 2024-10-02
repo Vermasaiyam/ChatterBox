f5febfe35797a783355cb4465bcac89988e7546f
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-02 13:15:15 +0530

    db connected

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,33 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from 'dotenv';
+import connectDB from "./utils/db.js";
+dotenv.config({});
 
 const PORT = process.env.PORT || 8000;
 const app = express();
+
+app.get('/', (req,res)=>{
+    return res.status(200).json({
+        message: "i m backend",
+        success: true,
+    })
+})
 
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+}
+app.use(cors(corsOptions));
 
-server.listen(PORT, () => {
-    // connectDB();
+
+app.listen(PORT, () => {
+    connectDB();
     console.log(`Server listen at port ${PORT}`);
 });
