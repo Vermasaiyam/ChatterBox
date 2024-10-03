953cc2eaba01c1794053c878f2459b3a0c0d63b9
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-03 20:36:59 +0530

    tailwind shadcn setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust paths according to your project structure
+  ],
+  theme: {
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+};
