6a7d224f3082956e5ecba2731e814a9d5f64d16c
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-05 21:59:26 +0530

    redux persist

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authSlice from './authSlice.js'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const rootReducer = combineReducers({
+  auth:authSlice,
+  post:postSlice,
+  socketio:socketSlice,
+  chat:chatSlice,
+  realTimeNotification:rtnSlice
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default configureStore({
-  reducer: {
-    auth: authSlice,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
