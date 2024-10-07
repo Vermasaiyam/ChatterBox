dd25926c69f628b53c7cfde65203f54ae8e7eb9e
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-07 21:45:16 +0530

    messages

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -5,7 +5,7 @@ import { setSelectedUser } from '@/redux/authSlice';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
 import { MessageCircleCode } from 'lucide-react';
-// import Messages from './Messages';
+import Messages from './Messages';
 import axios from 'axios';
 // import { setMessages } from '@/redux/chatSlice';
 import InitialsAvatar from 'react-initials-avatar';
@@ -79,7 +79,7 @@ const ChatPage = () => {
                               <span>{selectedUser?.username}</span>
                           </div>
                       </div>
-                      {/* <Messages selectedUser={selectedUser} /> */}
+                      <Messages selectedUser={selectedUser} />
                       <div className='flex items-center p-4 border-t border-t-gray-300'>
                           <Input value={textMessage} onChange={(e) => setTextMessage(e.target.value)} type="text" className='flex-1 mr-2 focus-visible:ring-transparent' placeholder="Messages..." />
                           <Button onClick={() => sendMessageHandler(selectedUser?._id)} className="text-2xl"><IoSend style={{ color: '#042035 !important' }} /></Button>
