e11f8cd764e721076c0b25684ed428a43e17dd24
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-07 22:15:36 +0530

    socket.io client using redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,10 @@ import MainLayout from './components/MainLayout'
 import Profile from './components/Profile'
 import Signup from './components/Signup'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { io } from "socket.io-client";
+import { setSocket } from './redux/socketSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { setOnlineUsers } from './redux/chatSlice'
 
 const browserRouter = createBrowserRouter([
   {
@@ -35,13 +39,13 @@ const browserRouter = createBrowserRouter([
         element:
           // <ProtectedRoutes>
           <EditProfile />
-          // </ProtectedRoutes>
+        // </ProtectedRoutes>
       },
       {
         path: '/chat',
         element: //<ProtectedRoutes>
           <ChatPage />
-         // </ProtectedRoutes>
+        // </ProtectedRoutes>
       },
     ]
   },
@@ -56,6 +60,39 @@ const browserRouter = createBrowserRouter([
 ])
 
 function App() {
+  const { user } = useSelector(store => store.auth);
+  const { socket } = useSelector(store => store.socketio);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (user) {
+      const socketio = io('http://localhost:8000', {
+        query: {
+          userId: user?._id
+        },
+        transports: ['websocket']
+      });
+      dispatch(setSocket(socketio));
+
+      // listen all the events
+      socketio.on('getOnlineUsers', (onlineUsers) => {
+        dispatch(setOnlineUsers(onlineUsers));
+      });
+
+      // socketio.on('notification', (notification) => {
+      //   dispatch(setLikeNotification(notification));
+      // });
+
+      // if closing the page then offline user
+      return () => {
+        socketio.close();
+        dispatch(setSocket(null));
+      }
+    } else if (socket) {
+      socket.close();
+      dispatch(setSocket(null));
+    }
+  }, [user, dispatch]);
 
   return (
     <>
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,6 +1,9 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import authSlice from './authSlice.js'
 import postSlice from './postSlice.js'
+import socketSlice from './socketSlice.js'
+import chatSlice from "./chatSlice.js";
+
 import {
   persistReducer,
   FLUSH,
@@ -21,6 +24,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
   auth:authSlice,
   post: postSlice,
+  socketio:socketSlice,
+  chat:chatSlice,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
