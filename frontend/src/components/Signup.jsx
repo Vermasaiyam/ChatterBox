782cad57bff4760ce594cd0029f305860df33248
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-21 21:22:33 +0530

    signup bug fixed

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -31,7 +31,7 @@ const Signup = () => {
         console.log(input);
         try {
             setLoading(true);
-            const result = await axios.post('https://chatterbox-aaxc.onrender.com/api/user/register', input, {
+            const res = await axios.post('https://chatterbox-aaxc.onrender.com/api/user/register', input, {
                 headers: {
                     'Content-Type': 'application/json'
                 },
